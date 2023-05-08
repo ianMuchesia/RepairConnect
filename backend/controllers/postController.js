1165d42174f9dad9b0beec962a271a714e06b8b0
ianMuchesia <ianmuchesia@yahoo.com>
2023-05-08 16:23:34 +0300

    repair mate

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,11 @@
 const Post = require("../models/Post");
 const { StatusCodes } = require("http-status-codes");
 const { checkPermission } = require("../utils");
-const { BadRequestError, NotFoundError, UnauthenticatedError } = require("../errors");
-
-
-
+const {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} = require("../errors");
 
 //post
 
@@ -22,166 +23,148 @@ const createPost = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ success: true });
 };
 
-
-
-   
 const getAllPosts = async (req, res) => {
   const posts = await Post.find({});
 
   res.status(StatusCodes.OK).json({ success: true, posts });
 };
 
+const getSinglePost = async (req, res) => {
+  const { id: postID } = req.params;
 
-const getSinglePost = async(req, res)=>{
-    const {id:postID} = req.params
+  const post = await findOne({ _id: postID });
+  if (!post) {
+    throw new NotFoundError(`no post found with id:${postID}`);
+  }
+  res.status(StatusCodes.OK).json({ success: true, post });
+};
 
-    const post = await findOne({_id:postID})
-    if(!post){
-        throw new NotFoundError(`no post found with id:${postID}`);
+const getSingleUserPosts = async (req, res) => {
+  const { userId } = req.user;
 
-    }
-    res.status(StatusCodes.OK).json({ success: true, post });
-}
+  const posts = await Post.find({ owner: userId });
 
+  res.status(StatusCodes.OK).json({ success: true, posts });
+};
 
-const getSingleUserPosts = async(req, res)=>{
-  const {userId} = req.user
+const deletePost = async (req, res) => {
+  const { id: postId } = req.params;
 
-  const posts = await Post.find({owner:userId})
+  checkPermission(req.user, req.user.userId);
+  const post = await Post.findOneAndRemove({ _id: postId });
 
-  res.status(StatusCodes.OK).json({success:true , posts})
-}
+  if (!post) {
+    throw new NotFoundError(`no post found with id:${postId}`);
+  }
 
-
-
-
-
-const deletePost = async(req, res)=>{
-    const { id: postId } = req.params;
-
-    checkPermission(req.user, req.user.userId);
-    const post = await Post.findOneAndRemove({_id:postId})
-
-    if (!post) {
-        throw new NotFoundError(`no post found with id:${postId}`);
-      }
-
-      res.status(StatusCodes.CREATED).json({success:true, post})
-}
-
-
+  res.status(StatusCodes.CREATED).json({ success: true, post });
+};
 
 //bids
 
-
-
 const createBid = async (req, res) => {
-    const { id: postID } = req.params;
-    const { userId: technicianId } = req.user;
-  
-    const { amount, bidMessage } = req.body;
-  
-  
-   
-    if (!amount || !bidMessage) {
-      throw new BadRequestError("Please provide all values");
-    }
-  
-    const post = await Post.findOne({ _id: postID });
-  
-    if (!post) {
-      throw new NotFoundError(`no post found with id:${postID}`);
-    }
-  
-    post.bids.push({
-      technician: technicianId,
-      amount,
-      bidMessage: bidMessage,
-    });
-  
-  
-    await post.save(); 
-  
-    res.status(StatusCodes.CREATED).json({success:true})
-  };
-  
+  const { id: postID } = req.params;
+  const { userId: technicianId } = req.user;
 
+  const { amount, bidMessage } = req.body;
 
+  if (!amount || !bidMessage) {
+    throw new BadRequestError("Please provide all values");
+  }
 
-const getAllBids = async(req , res)=>{
-    
-    const {id:postID} = req.params
-    const {userId} = req.user
-    checkPermission(req.user , userId)
+  const post = await Post.findOne({ _id: postID });
 
-    const bids = await Post.findOne({_id:postID, owner:userId}).select('bids')
+  if (!post) {
+    throw new NotFoundError(`no post found with id:${postID}`);
+  }
 
-    if(!bids){
-        throw new NotFoundError(`no post found with id:${postID}`); 
-    }
+  post.bids.push({
+    technician: technicianId,
+    amount,
+    bidMessage: bidMessage,
+  });
 
-    res.status(StatusCodes.OK).json({success:true, bids})
+  await post.save();
 
+  res.status(StatusCodes.CREATED).json({ success: true });
+};
 
-} 
+const getAllBids = async (req, res) => {
+  const { id: postID } = req.params;
+  const { userId } = req.user;
+  checkPermission(req.user, userId);
 
+  const bids = await Post.findOne({ _id: postID, owner: userId }).select(
+    "bids"
+  );
 
+  if (!bids) {
+    throw new NotFoundError(`no post found with id:${postID}`);
+  }
+
+  res.status(StatusCodes.OK).json({ success: true, bids });
+};
 
 const getSingleBid = async (req, res) => {
-    const { id: postID, bidID } = req.params;
+  const { id: postID, bidID } = req.params;
 
+  checkPermission(req.user, req.user.userId);
+  const post = await Post.findOne({
+    _id: postID,
+    $or: [
+      { owner: req.user.userId },
+      { bids: { $elemMatch: { technician: req.user.userId } } },
+    ],
+  }).select("bids");
 
-   
-    checkPermission(req.user, req.user.userId);
-    const post = await Post.findOne({
-      _id: postID,
-      $or: [
-        { owner: req.user.userId },
-        { bids: { $elemMatch: { technician: req.user.userId } } }
-      ]
-    }).select('bids');
-    
-    if (!post) {
-      throw new NotFoundError(`No post found with id: ${postID}`);
-    }
-    
-    // Find the bid that matches the bidID parameter
-    const bid = post.bids.find(bid => bid._id.toString() === bidID);
-    
-    if (!bid) {
-      throw new NotFoundError(`No bid found with id: ${bidID} on post ${postID}`);
-    }
-    
-    // Check if the bid belongs to the technician making the request
-    if (bid.technician.toString() !== req.user.userId) {
-      throw new UnauthenticatedError(`You are not authorized to access this bid`);
-    }
-    
-    res.status(StatusCodes.OK).json({ success: true, bid });
-    
-  };
-  
+  if (!post) {
+    throw new NotFoundError(`No post found with id: ${postID}`);
+  }
 
+  // Find the bid that matches the bidID parameter
+  const bid = post.bids.find((bid) => bid._id.toString() === bidID);
 
+  if (!bid) {
+    throw new NotFoundError(`No bid found with id: ${bidID} on post ${postID}`);
+  }
 
-const deleteBid = async(req, res)=>{
-    const { id: postId, bidID } = req.params;
- 
+  // Check if the bid belongs to the technician making the request
+  if (bid.technician.toString() !== req.user.userId) {
+    throw new UnauthenticatedError(`You are not authorized to access this bid`);
+  }
 
-    checkPermission(req.user , req.user.userId)
+  res.status(StatusCodes.OK).json({ success: true, bid });
+};
 
-    const post = await Post.findOneAndUpdate(
-        
-        {_id:postId, bids: { $elemMatch: { technician: req.user.userId, _id:bidID } } },
-       { $pull: {bids:{_id:bidID}}},
-       { projection: { bids: { $elemMatch: { _id: bidID } } } }
+const deleteBid = async (req, res) => {
+  const { id: postId, bidID } = req.params;
 
-    )
-    // The $ is a placeholder that tells Mongoose to return the matched array element.
+  checkPermission(req.user, req.user.userId);
 
-    if(!post){
-        throw new NotFoundError(`no post found with id:${postId}`)
-    }
-    res.status(StatusCodes.OK).json({success:true, msg:"Bid Deleted"})
-}
-module.exports = { getAllPosts,getSinglePost, createPost , createBid, deletePost, deleteBid, getSingleBid ,getAllBids , getSingleBid, getSingleUserPosts};
+  const post = await Post.findOneAndUpdate(
+    {
+      _id: postId,
+      bids: { $elemMatch: { technician: req.user.userId, _id: bidID } },
+    },
+    { $pull: { bids: { _id: bidID } } },
+    { projection: { bids: { $elemMatch: { _id: bidID } } } }
+  );
+  // The $ is a placeholder that tells Mongoose to return the matched array element.
+
+  if (!post) {
+    throw new NotFoundError(`no post found with id:${postId}`);
+  }
+  res.status(StatusCodes.OK).json({ success: true, msg: "Bid Deleted" });
+};
+module.exports = {
+  getAllPosts,
+  getSinglePost,
+  createPost,
+  createBid,
+  deletePost,
+  deleteBid,
+  getSingleBid,
+  getAllBids,
+  getSingleBid,
+  getSingleUserPosts,
+};
